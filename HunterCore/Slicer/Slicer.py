942e4497abc96fbd7627bd1749ba3bec6bd3c66f
Magister Machinis <30908581+Magister-Machinis@users.noreply.github.com>
2020-08-09 12:24:23 -0400

    working on wrapper for the unit tests

--- HunterCore/Slicer/Slicer.py
+++ HunterCore/Slicer/Slicer.py
@@ -2,19 +2,14 @@ import Searcher
 import os
 from multiprocessing import Pool
 
-# slicing rule structure
-#{
-#    rule: as per searcher,
-#    key: key split on if search match,
-#    specific: whether to run general match or specific match
-#}
 
 
-def slicing(file, rule):
+
+def _slicing(file, rule):
     flag = False
     searchpattern = Searcher.searcher(rule['rule'])
     with open(file,"r") as f:
-        data =f.read()
+        data =json.loads(f.read())
         if rule['specific']:
             if searchpattern.SpecificMatch(data):
                 flag=True
@@ -22,10 +17,10 @@ def slicing(file, rule):
             if searchpattern.IsMatch(data):
                 flag=True
         if flag== True:            
-             for x in slicebykeys(rule['key'],data):
+             for x in _slicebykeys(rule['key'],data):
                 yield x
 
-def slicebykeys(key, data):
+def _slicebykeys(key, data):
     for datakey in data.keys():
         if datakey==key:
             if isinstance(data[datakey],list) or isinstance(data[datakey],tuple):
@@ -39,26 +34,37 @@ def slicebykeys(key, data):
         else:
             if isinstance(data[datakey],list) or isinstance(data[datakey],tuple):
                 for item in data[datakey]:
-                    for result in slicebykeys(key,item):
+                    for result in _slicebykeys(key,item):
                         yield result
             elif isinstance(data[datakey],dict):
                 for itemkey in data[datakey].keys():
-                    for result in slicebykeys(key, data[datakey][itemkey]):
+                    for result in _slicebykeys(key, data[datakey][itemkey]):
                         yield result
 
-def slicewriter(file, rule,intermediatepath):
+def _slicewriter(file, rule,intermediatepath):
     count = 0
-    for result in slicing(file, rule):
+    for result in _slicing(file, rule):
         with open(os.path.join(intermediatepath, str(count)+file),'w') as writer:
             writer.write(result)
         count= count+1
 
-def Slicer(rules=[],poolsize=8):
+def Slicer(rules=[],poolsize=8,debug=False):
+    """ 
+    poolsize to be size of threadpool
+    _slicing rule structure
+    [
+    {
+    rule: as per searcher,
+    key: key split on if search match,
+    specific: whether to run general match or specific match
+    },
+    ]
+    """
     inputpath = os.path.abspath("../Input")
     intermediatepath = os.path.abspath("../Intermediate")
     with Pool(poolsize) as pool:
         for file in os.listdir(inputpath):
             if file.endswith('.json'):
                 for rule in rules:
-                    pool.apply_async(slicewriter, args=(file, rule, intermediatepath))
+                    pool.apply_async(_slicewriter, args=(file, rule, intermediatepath))
         
--- HunterCore/Slicer/SlicerTest.py
+++ HunterCore/Slicer/SlicerTest.py
@@ -1,9 +1,42 @@
 import unittest
-import Sicer
+import Slicer
+import json
+import os
+import functools
+
+
+def testdatawrapper (func):
+    @functools.wraps(func)
+    def wrapper(*args,**kwargs):
+        #sample json chunk for testing purposes
+        examplejson= """{"menu": {
+      "id": "file",
+      "value": "File",
+      "popup": {
+        "menuitem": [
+          {"value": "New", "onclick": "CreateNewDoc()"},
+          {"value": "Open", "onclick": "OpenDoc()"},
+          {"value": "Close", "onclick": "CloseDoc()"}
+        ]
+      }
+    }}"""
+    
+        examplejsondes=json.loads(examplejson)
+            
+        with open("Input/Test.json",'w') as testfile:
+            json.dump(examplejsondes,testfile)
+        results = func(*args, **kwargs)
+        os.remove("Input/Test.json")
+        return results
+    return wrapper
+
 
 class Test_SlicerTest(unittest.TestCase):
-    def test_A(self):
-        self.fail("Not implemented")
+    
+    @testdatawrapper
+    def TestforExplicitErrors(self):
+        Slicer.Slicer(rules=[{rule:"menuitem",key:"menuitem",specific:False}], poolsize=6, debug=True)
+    
 
 if __name__ == '__main__':
     unittest.main()
