9f07e92c0320cef2b5fa830a65785715b4fdcb23
Magister Machinis <30908581+Magister-Machinis@users.noreply.github.com>
2020-08-10 08:55:42 -0400

    setting up debug messages

--- HunterCore/Slicer/SlicerTest.py
+++ HunterCore/Slicer/SlicerTest.py
@@ -5,38 +5,54 @@ import os
 import functools
 
 
-def testdatawrapper (func):
-    @functools.wraps(func)
-    def wrapper(*args,**kwargs):
-        #sample json chunk for testing purposes
-        examplejson= """{"menu": {
-      "id": "file",
-      "value": "File",
-      "popup": {
-        "menuitem": [
-          {"value": "New", "onclick": "CreateNewDoc()"},
-          {"value": "Open", "onclick": "OpenDoc()"},
-          {"value": "Close", "onclick": "CloseDoc()"}
-        ]
-      }
-    }}"""
+def testdatawrapper (debug):
+    if debug:
+        print('debug enabled')
+        print(os.getcwd())
+    def funclayer(func):
+        @functools.wraps(func)
+        def wrapper(*args,**kwargs):
+            #sample json chunk for testing purposes
+            examplejson= """{"menu": {
+          "id": "file",
+          "value": "File",
+          "popup": {
+            "menuitem": [
+              {"value": "New", "onclick": "CreateNewDoc()"},
+              {"value": "Open", "onclick": "OpenDoc()"},
+              {"value": "Close", "onclick": "CloseDoc()"}
+            ]
+          }
+        }}"""
     
-        examplejsondes=json.loads(examplejson)
-            
-        with open("Input/Test.json",'w') as testfile:
-            json.dump(examplejsondes,testfile)
-        results = func(*args, **kwargs)
-        os.remove("Input/Test.json")
-        return results
-    return wrapper
+            examplejsondes=json.loads(examplejson)
+            if debug==True:
+                print(examplejsondes)
+            with open("../Input/Test.json",'w') as testfile:
+                json.dump(examplejsondes,testfile)
+            if debug:
+                print(os.listdir('../Input'))
+            results = func( debug=debug, *args, **kwargs)
+            if debug==True:
+                print(results)
+            return results
+        return wrapper
+    try:
+        return funclayer
+    except Exception as e:
+        throw(e)
+        if debug:
+            print(e)
+    finally:
+        os.remove("../Input/Test.json")
 
 
 class Test_SlicerTest(unittest.TestCase):
     
-    @testdatawrapper
+    @testdatawrapper(debug=True)
     def TestforExplicitErrors(self):
-        Slicer.Slicer(rules=[{rule:"menuitem",key:"menuitem",specific:False}], poolsize=6, debug=True)
-    
+        Slicer.Slicer(rules=[{rule:"menuitem",key:"menuitem",specific:False}], poolsize=6)
+        self.assertEqual(len([name for name in os.listdir('../Intermediate') if os.path.isfile(name)]),3)
 
 if __name__ == '__main__':
     unittest.main()
